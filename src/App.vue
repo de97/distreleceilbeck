a6c1297fea327666da781ef40b09418d37eea1f6
de97 <danielkofi@hotmail.co.ul>
2020-12-01 14:14:12 +0000

    fifth commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <container>
-    <h1>Related Products</h1>
-      <VueSlickCarousel :dots="true" :slidesToShow=4 :slidesToScroll=4>
+    <div class="container">
+      <h1>Related Products</h1>
+      <VueSlickCarousel v-bind="settings">
         <div
           class="relproduct__id"
           v-for="product in products.carouselData"
           :key="product.code"
         >
           <div class="relproduct__content">
-            <!-- <img class=relproduct__img src="https://www.distrelec.biz/Web/WebShopImages/landscape_medium/ho/to/SO-16W-Photo.jpg"> -->
-            <img class=relproduct__img src= ${product.productImageUrl}>
+            <img
+              class="relproduct__image"
+              style="max-width: 20%"
+              :src="product.productImageUrl"
+            />
             <p class="relproduct__name">
               {{ product.name }}
             </p>
             <ul class="relpoduct__list">
-              <li class="relproduct__item">Stock {{product.itemPositionOneBased}}</li>
-              <li class="relproduct__item">{{product.price.currency}} {{product.price.formattedValue}}</li>
+              <li class="relproduct__item">
+                Stock {{ product.itemPositionOneBased }}
+              </li>
+              <li class="relproduct__item">
+                {{ product.price.currency }} {{ product.price.formattedValue }}
+              </li>
             </ul>
-            <button class="relproduct__button">Buy now</button>
+            <a class="relproduct_link" :href="product.url">Buy Now</a>
           </div>
         </div>
       </VueSlickCarousel>
-    </container>
+    </div>
   </div>
 </template>
 
@@ -40,10 +47,40 @@ export default {
   data() {
     return {
       products: relProdData,
+      settings: {
+        arrows: true,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+       
+        responsive: [
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: false
+            }
+          }
+
+        ],
+      },
     };
   },
 };
 </script>
-
-
-//<span class="relproduct__img"> {{ product.url }}</span>
